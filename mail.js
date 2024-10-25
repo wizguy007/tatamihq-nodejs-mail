087a5fecbd43b94bbb216919d813a4367edd33b5
Emmanuel Osuh <mrmusonant@gmail.com>
2024-10-25 13:40:56 +0100

    Revert unnecessary linting

--- mail.js
+++ mail.js
@@ -1,11 +1,11 @@
-const nodemailer = require("nodemailer");
-const hbs = require("nodemailer-express-handlebars");
-const path = require("path");
-require("dotenv").config();
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+const path = require('path');
+require('dotenv').config();
 
 // Step 3: Configure the transporter (Replace with your SMTP service)
 let transporter = nodemailer.createTransport({
-  service: "mailgun", // or use your own email service
+  service: 'mailgun', // or use your own email service
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
   tls: true,
@@ -18,17 +18,17 @@ let transporter = nodemailer.createTransport({
 // Step 4: Setup handlebars engine options
 const handlebarOptions = {
   viewEngine: {
-    extName: ".handlebars",
-    partialsDir: path.resolve("./templates/partials"), // Directory for partials
-    layoutsDir: path.resolve("./templates/layouts"), // Directory for layouts
-    defaultLayout: "main", // Default layout file
+    extName: '.handlebars',
+    partialsDir: path.resolve('./templates/partials'), // Directory for partials
+    layoutsDir: path.resolve('./templates/layouts'), // Directory for layouts
+    defaultLayout: 'main', // Default layout file
   },
-  viewPath: path.resolve("./templates"),
-  extName: ".handlebars",
+  viewPath: path.resolve('./templates'),
+  extName: '.handlebars',
 };
 
 // Step 5: Use handlebars template engine with nodemailer
-transporter.use("compile", hbs(handlebarOptions));
+transporter.use('compile', hbs(handlebarOptions));
 
 // Step 6: Define the email options
 const mailOptions = {
@@ -36,15 +36,15 @@ const mailOptions = {
     name: process.env.MAIL_FROM_ADDRESS_NAME,
     address: process.env.MAIL_FROM_ADDRESS,
   },
-  to: "mrmusonant@gmail.com",
-  subject: "Test Email with Handlebars Template",
-  template: "welcome", // name of the template file (without extension)
+  to: 'mrmusonant@gmail.com',
+  subject: 'Test Email with Handlebars Template',
+  template: 'welcome', // name of the template file (without extension)
   context: {
-    title: "Welcome to Tatami",
-    heading: "Welcome to Tatami",
-    name: "John",
+    title: 'Welcome to Tatami',
+    heading: 'Welcome to Tatami',
+    name: 'John',
     // message: "This is a test email using Handlebars template engine.",
-    loginUrl: "#",
+    loginUrl: '#',
   },
 };
 
@@ -53,5 +53,5 @@ transporter.sendMail(mailOptions, (error, info) => {
   if (error) {
     return console.log(error);
   }
-  console.log("Email sent: " + info.response);
+  console.log('Email sent: ' + info.response);
 });
